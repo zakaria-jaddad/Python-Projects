ce239ca721eb497961093c84eb3b4d0bcecfd64b
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2023-05-03 11:20:34 +0100

    getting started

--- pomodoro/app.py
+++ pomodoro/app.py
@@ -1,3 +1,13 @@
+# after i heva practiced a bit What Do I Want to make 
+    # ? user chose or add it's own sound if the user didn't chose one there is the deafult 
+    # ? user chose how long the pomodoro times will take 
+    # ? user chose how long the short braek and long one would take 
+    # ? once every secion ended the user hear the shosen sound 
+    # ? user see what type of secion [pomodoro, short break, long break]
+    # ? user can see the clock, how much time left sor the next secion 
+    # ? and a slider that have a percentage calculates the progress of the secion 
+        # TODO : THAT IS ALL :)
+
 import datetime
 
 from termcolor import colored
@@ -8,38 +18,4 @@ import time
 
 import sys
 
-# play_sound('suii.mp3')
 
-print(colored('worked', 'green', 'on_grey'))
-
-    # TODO : Just for knowing the bass of this i'll make first a stopwatch timer 
-seconds = 0
-minutes = 0
-hours = 0
-
-while True:
-
-    move_to_start()
-
-    # clear_line()
-
-    sys.stdout.write(formate(hours, minutes, seconds))
-    sys.stdout.flush()
-
-    time.sleep(1)
-
-    # * seconds == 60 it means 1 minute 
-
-    if seconds == 60:
-
-        seconds = -1
-        # add one minute 
-        minutes  += 1
-
-        # * hour
-        if minutes == 60:
-            hours += 1
-            minutes = 0
-
-    # * incremant seconds in every iteration 
-    seconds += 1
