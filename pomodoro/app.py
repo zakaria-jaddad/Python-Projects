435310f5481f46c4567097223b83817c7780d64c
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2023-05-03 15:08:29 +0100

    that was cool most of things are done i just need to add the clock

--- pomodoro/app.py
+++ pomodoro/app.py
@@ -26,12 +26,22 @@ chosen_sound = None
 
 SOUND = ['suii.mp3', 'twitch.mp3', 'rock.mp3']
 
+
 def main():
+
+    SECION_COUNTER = 1
+
     try:
-        pomodoro = int(sys.argv[1])
-        time_break = int(sys.argv[2])
+        POMODORO = int(sys.argv[1])
+        BREAK_TIME = int(sys.argv[2])
+
+        # ! checking if there is any negative or zero value 
+        if POMODORO < 1 or BREAK_TIME < 1:
+            print(colored("Please Write A Positive Number Try Again...", 'red'))            
+            sys.exit()
     except ValueError:
         print(colored("Write A Number Not A String You Morone", 'red'))
+        sys.exit()
 
     foo = input("> Press 'E' For Settings ").upper()
 
@@ -70,7 +80,35 @@ def main():
 
     else:
         chosen_sound = SOUND[1]
+
+
+    # * after all the promodoro will start here 
+    while True:
+        # * Working 
+        if SECION_COUNTER % 2 != 0:
+            print(colored("Work Time !", 'cyan'))
+
+            for n in track(range(30), description="Work..."):
+                time.sleep(1)
+
+        # * Short Break
+        elif SECION_COUNTER % 2 == 0 and SECION_COUNTER % 8 != 0:
+            print(colored("Short Break", 'red'))
+
+            for n in track(range(30), description="Break..."):
+                time.sleep(1)
+
+        # * Long Break 
+        else:
+            print(colored("Long Break", 'magenta')) 
+
+            for n in track(range(15), description="Nap..."):
+                time.sleep(1)
+
+
         play_sound(chosen_sound)
+        SECION_COUNTER +=1
+
 
 
     
@@ -84,3 +122,18 @@ if __name__ == '__main__':
 # for n in track(range(20), description="Processing..."):
 #     # ! n here represent seconds 
 #     time.sleep(n * 0.1)
+
+
+# ! os we have 4 promodomos to get the long break and evry time a promodomo section ends a short break starts 
+#   ! so this is like this {
+        # ! 1 : work, 
+        # ! 2 : short break     
+        # ! 3 : work, 
+        # ! 4 : short break  
+        # ! 5 : work, 
+        # ! 6 : short break     
+        # ! 7 : work, 
+        # ! 8 : long break     
+# ! }
+    # ! as you can see in this list the section follow allong like this 
+        # ! it's just a simple counter wiht some conditions and you can goo wiht it 
